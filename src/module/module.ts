6181b77d7ea4056902be69cd4c365d95be73c94f
Isabel Maskrey <isabel@satyrn.dev>
2020-07-27 12:14:50 -0400

    🏷 Pass logger to Module on instantiation

--- src/module/module.ts
+++ src/module/module.ts
@@ -2,34 +2,39 @@ import { Command } from './command';
 import { EventHandler } from './event-handler';
 import { ModuleInfo } from './module-info';
 import { Initializeable } from '../utils';
+import { injectable } from 'inversify';
+import { LoggingService, Logger } from '../services';
 
 /**
  * The base class for a module implementation.
  */
+@injectable()
 export abstract class Module implements Initializeable {
-    /**
-     * A list of commands which are provided by this module.
-     */
-    public readonly commands: Command[];
+  /**
+   * A list of commands which are provided by this module.
+   */
+  public readonly commands: Command[];
 
-    /**
-     * A list of events which should be registered to this module.
-     */
-    public readonly events: EventHandler[];
+  /**
+   * A list of events which should be registered to this module.
+   */
+  public readonly events: EventHandler[];
 
-    /**
-     * @param moduleInfo The parsed information from the associated module info file.
-     */
-    constructor(public readonly moduleInfo: ModuleInfo) { }
+  /**
+   * @param moduleInfo The parsed information from the associated module info file.
+   */
+  constructor(public readonly moduleInfo: ModuleInfo,
+    protected readonly log: Logger) {
+  }
 
-    public abstract async preInitialize() : Promise<void>;
+  public abstract async preInitialize(): Promise<void>;
 
-    public abstract async initialize() : Promise<void>;
+  public abstract async initialize(): Promise<void>;
 
-    public abstract async postInitialize() : Promise<void>;
+  public abstract async postInitialize(): Promise<void>;
 
-    /**
-     * Registers any dependencies to the IOC container
-     */
-    public abstract async registerDependencies(): Promise<void>;
+  /**
+   * Registers any dependencies to the IOC container
+   */
+  public abstract async registerDependencies(): Promise<void>;
 }
