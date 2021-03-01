f8b28aaa8128edb926e07563693ae021bef5fac0
Isabel Maskrey <isabel@satyrn.dev>
2021-03-01 11:25:05 -0500

    🚑 module methods to async

--- src/services/module-service.ts
+++ src/services/module-service.ts
@@ -10,23 +10,23 @@ export interface IModuleService extends IInitializeable {
   /**
    * Gets a list of all loaded modules.
    */
-  getAllModules(guild?: Guild): OneOrMany<Module>;
+  getAllModules(guild?: Guild): Promise<OneOrMany<Module>>;
 
   /**
    * Gets a single module by ID
    * @param moduleId The module ID to search for.
    */
-  getModuleById(moduleId: string, guild?: Guild): Module;
+  getModuleById(moduleId: string, guild?: Guild): Promise<Module>;
 
   /**
    * Gets a module or modules by name.
    * @param moduleName The module name to search for.
    */
-  getModulesByName(moduleName: string, guild?: Guild): OneOrMany<Module>;
+  getModulesByName(moduleName: string, guild?: Guild): Promise<OneOrMany<Module>>;
 
   /**
    * Gets a module or modules by name or ID.
    * @param moduleIdOrName The module ID or module name to search for.
    */
-  getModulesByIdOrName(moduleIdOrName: string, guild?: Guild): OneOrMany<Module>;
+  getModulesByIdOrName(moduleIdOrName: string, guild?: Guild): Promise<OneOrMany<Module>>;
 }
