4b304ce005e83588f0d75e7382158a551085d5ff
Isabel Maskrey <isabel@satyrn.dev>
2021-03-01 10:06:39 -0500

    ✨ Per-guild module availability

--- src/services/module-service.ts
+++ src/services/module-service.ts
@@ -1,5 +1,6 @@
 import { Module } from '../module';
 import { IInitializeable, OneOrMany } from '@satyrnidae/apdb-utils';
+import { Guild } from 'discord.js';
 
 /**
  * A service which provides access to all loaded modules
@@ -9,23 +10,23 @@ export interface IModuleService extends IInitializeable {
   /**
    * Gets a list of all loaded modules.
    */
-  getAllModules(): OneOrMany<Module>;
+  getAllModules(guild?: Guild): OneOrMany<Module>;
 
   /**
    * Gets a single module by ID
    * @param moduleId The module ID to search for.
    */
-  getModuleById(moduleId: string): Module;
+  getModuleById(moduleId: string, guild?: Guild): Module;
 
   /**
    * Gets a module or modules by name.
    * @param moduleName The module name to search for.
    */
-  getModulesByName(moduleName: string): OneOrMany<Module>;
+  getModulesByName(moduleName: string, guild?: Guild): OneOrMany<Module>;
 
   /**
    * Gets a module or modules by name or ID.
    * @param moduleIdOrName The module ID or module name to search for.
    */
-  getModulesByIdOrName(moduleIdOrName: string): OneOrMany<Module>;
+  getModulesByIdOrName(moduleIdOrName: string, guild?: Guild): OneOrMany<Module>;
 }
