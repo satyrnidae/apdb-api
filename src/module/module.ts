ffe6e4b62772da2da8d76ba961244e4f1f615cff
Isabel Maskrey <isabel@satyrn.dev>
2021-03-05 22:43:46 -0500

    ♻️ Remove async abstract methods (what the heck)

--- src/module/module.ts
+++ src/module/module.ts
@@ -13,12 +13,12 @@ export abstract class Module implements IInitializeable {
   /**
    * A list of commands which are provided by this module.
    */
-  public readonly commands: Command[];
+  public abstract commands: Command[];
 
   /**
    * A list of events which should be registered to this module.
    */
-  public readonly events: EventHandler[];
+  public abstract events: EventHandler<any>[];
 
   /**
    * @param moduleInfo The parsed information from the associated module info file.
@@ -27,14 +27,14 @@ export abstract class Module implements IInitializeable {
     protected readonly log: Logger) {
   }
 
-  public abstract async preInitialize(): Promise<void>;
+  public abstract preInitialize(): Promise<void>;
 
-  public abstract async initialize(): Promise<void>;
+  public abstract initialize(): Promise<void>;
 
-  public abstract async postInitialize(): Promise<void>;
+  public abstract postInitialize(): Promise<void>;
 
   /**
    * Registers any dependencies to the IOC container
    */
-  public abstract async registerDependencies(): Promise<void>;
+  public abstract registerDependencies(): Promise<void>;
 }
